37a2cc029251ceb1c32242da4016cf0711e2d6f4
k-totani <totani@blue-corporation.jp>
2015-09-06 00:54:19 +0900

    apiリファクタリング

--- api/get_result.php
+++ api/get_result.php
@@ -14,20 +14,22 @@ try {
     $res = array();
     $stasus = 200;
 
-    $stmt = $pdo-> prepare("SELECT COUNT( * ) FROM $user_table");
+    $stmt = $pdo-> prepare("SELECT COUNT( * ) as row_cnt FROM $user_table");
     $success = $stmt->execute();
     if ($success) {
-        $sql = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $data = $sql;
-        $user_sum = $data[0]["COUNT( * )"];
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        $user_sum = $data["row_cnt"];
 
         for ($i = 1; $i <= $user_sum; $i++) {
-            $stmt = $pdo-> prepare("SELECT *,count( * ) FROM $result_table RIGHT JOIN $user_table ON $result_table.LOSER = $user_table.ID where WINNER=$parameter AND LOSER=$i");
+            $stmt = $pdo-> prepare("SELECT *,count( * ) as win_cnt FROM $result_table RIGHT JOIN $user_table ON $result_table.LOSER = $user_table.ID where WINNER=$parameter AND LOSER=$i");
             $stmt->execute();
-            $sql = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            $data = $sql;
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
             $success = $stmt->execute();
-            $result = array('win_sum'=> $data[0]["count( * )"],'rival_id'=> $i,'rival_name'=>$data[0]["NAME"]);
+            $result = [
+                'win_sum'=> $data["win_cnt"],
+                'rival_id'=> $i,
+                'rival_name'=>$data["NAME"]
+                ];
             array_push( $res, $result );
 
         }
