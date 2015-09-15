786d8142fdb08f2b7ae7c4b660a0ffbdc3186e58
k-totani <totani@blue-corporation.jp>
2015-09-16 01:46:19 +0900

    ver.1: result_api変更

--- api/get_result.php
+++ api/get_result.php
@@ -6,7 +6,8 @@ try {
     $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $db . ';charset=utf8' , $user , $pass,
     array(PDO::ATTR_EMULATE_PREPARES => false));
 
-    $param = $_GET['user'];
+    $user_param = $_GET['user'];
+    $rival_param = $_GET['rival'];
 
     // 保存
     $user_table = "USER";
@@ -14,36 +15,49 @@ try {
     $res = array();
     $stasus = 200;
 
-    $stmt = $pdo-> prepare("SELECT COUNT( * ) as row_cnt FROM $user_table");
+    $stmt = $pdo-> prepare("SELECT * FROM $user_table");
     $success = $stmt->execute();
-    if ($success) {
-        $data = $stmt->fetch(PDO::FETCH_ASSOC);
-        $user_sum = $data["row_cnt"];
+    $user_list = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        for ($i = 1; $i <= $user_sum; $i++) {
-            $stmt = $pdo-> prepare("SELECT *,count(*) as cnt
-            FROM RESULT LEFT JOIN USER ON RESULT.LOSER = USER.ID
-            where WINNER=$param AND LOSER=$i or WINNER=$i AND LOSER=$param
-            group by WINNER, LOSER");
+    $stmt = $pdo-> prepare("SELECT *, count(*) as cnt
+    FROM RESULT
+    where WINNER=$user_param AND LOSER=$rival_param
+    ");
+
 
             $stmt->execute();
-            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $win_data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $stmt = $pdo-> prepare("SELECT *, count(*) as cnt
+    FROM RESULT
+    where WINNER=$rival_param AND LOSER=$user_param
+    ");
+
+            $stmt->execute();
+            $lose_data = $stmt->fetch(PDO::FETCH_ASSOC);
+
             // var_dump($data);
             // exit();
             $result = [
-                'rival_id'=> $i,
-                'win_sum'=> $data[0]["cnt"],
-                'lose_sum'=> $data[1]["cnt"],
-                'rival_name'=>$data[0]["NAME"]
+                'user_id'=> $user_param,
+                'rival_id'=> $rival_param,
+                'win_sum'=> $win_data["cnt"],
+                'lose_sum'=> $lose_data["cnt"],
+                'my_name'=>$user_list[$user_param]["NAME"],
+                'rival_name'=>$user_list[$rival_param]["NAME"]
                 ];
-            array_push( $res, $result );
+            $stasus = 200;
+            $data = $sql;
+            $res =  array('status'=>$stasus, 'data'=>$result);
+            header('Content-type: application/json');
+            // array_push( $res, $result );
 
-        }
-    } else {
-        $stasus = 400;
-        $error = "DB参照エラー: USER TABLE";
-        $res =  array('status'=> $stasus,'error'=> $error);
-    }
+    //     }
+    // } else {
+    //     $stasus = 400;
+    //     $error = "DB参照エラー: USER TABLE";
+    //     $res =  array('status'=> $stasus,'error'=> $error);
+    // }
 
     echo json_encode( $res );
 
