55db9730b4d6200f1de0b953fe8f4fddd8ca21cd
Kohei Totani <syshayr_soccer_46@yahoo.co.jp>
2016-01-07 02:25:16 +0900

    アプリ起動時とフォアグランドには行った時に自動ロックさせないように変更

--- ios/Magic/Magic/AppDelegate.m
+++ ios/Magic/Magic/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
+    [UIApplication sharedApplication].idleTimerDisabled = YES;
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = [[UIViewController alloc] init];
     self.window.backgroundColor = [UIColor whiteColor];
@@ -38,12 +40,13 @@
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [UIApplication sharedApplication].idleTimerDisabled = NO;
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [UIApplication sharedApplication].idleTimerDisabled = YES;
+
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
